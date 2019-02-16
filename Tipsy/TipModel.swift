f2acdbe29c57fb75be294d73d005e360033f1227
Julian Mino <julianmino@Julians-Mac.local>
2019-02-16 03:32:22 -0300

    coded the Model part

---
+++ Tipsy/TipModel.swift
@@ -0,0 +1,57 @@
+//
+//  TipModel.swift
+//  Tipsy
+//
+//  Created by Julian Mino on 2/16/19.
+//  Copyright © 2019 Julian Mino. All rights reserved.
+//
+
+import Foundation
+
+class TipModel {
+    private var _billAmount: Double = 0
+    private var _tipPercent: Double = 0
+    private var _tipAmount: Double = 0
+    private var _totalAmount: Double = 0
+    
+    var billAmount: Double {
+        get {
+            return _billAmount
+        }
+        set {
+            _billAmount = newValue
+        }
+    }
+    
+    var tipPercent: Double {
+        get {
+            return _tipAmount
+        }
+        set {
+            _billAmount = newValue
+        }
+    }
+    
+    var tipAmount: Double {
+        
+            return _tipAmount
+    }
+    
+    var totalAmount: Double {
+        
+            return _totalAmount
+    }
+    
+
+    init(billAmount: Double, tipPercent: Double) {
+        self._billAmount = billAmount
+        self._tipPercent = tipPercent
+    }
+    
+    
+    func calculateTip () {
+        _tipAmount = billAmount * tipPercent
+        _totalAmount = billAmount + tipAmount
+    }
+    
+}
